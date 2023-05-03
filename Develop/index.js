c958e008a55cc5248c42ae771985033b372a7612
bentwhiskers <abbott1414@gmail.com>
2023-05-03 17:57:57 -0400

    added packages needed for app, created array of questions for user input. started creating function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,67 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'Enter items for table of contents:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter instructions for installation:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter instructions for how to use app:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Pick project license:',
+        choices: ['None', 'MIT', 'BSD','GPL', 'Apache']
+
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter the testing instructions:'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const fileName = ''
+}
 
 // TODO: Create a function to initialize app
 function init() {}
